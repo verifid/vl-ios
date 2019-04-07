7ddd20c4cdf42ab8743137c5e7072acef134d011
Abdullah Selek <abdullahselek@gmail.com>
2019-04-07 16:59:07 +0100

    Create empty project.

---
+++ vl-ios/vl_ios.h
@@ -0,0 +1,19 @@
+//
+//  vl_ios.h
+//  vl-ios
+//
+//  Created by Abdullah Selek on 07.04.19.
+//  Copyright © 2019 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for vl_ios.
+FOUNDATION_EXPORT double vl_iosVersionNumber;
+
+//! Project version string for vl_ios.
+FOUNDATION_EXPORT const unsigned char vl_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vl_ios/PublicHeader.h>
+
+
