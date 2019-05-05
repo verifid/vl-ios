b157300a2ac1518f03adda6cc2af0350135cddfe
Abdullah Selek <abdullahselek@gmail.com>
2019-05-05 19:55:33 +0100

    Add stubbing dependency and test for APIClient.

--- Podfile
+++ Podfile
@@ -1,14 +1,23 @@
 platform :ios, '11.0'
+use_frameworks!
 
 def product_pods
     pod 'VLiOS', :path => '.'
 end
 
 workspace 'vl-ios.xcworkspace'
-project 'Sample/Sample.xcodeproj'
+
+target 'vl-ios' do
+    project 'vl-ios.xcodeproj'
+
+    target 'vl-iosTests' do
+        inherit! :search_paths
+        pod 'OHHTTPStubs/Swift'
+    end
+end
 
 target 'Sample' do
-    use_frameworks!
+    project 'Sample/Sample.xcodeproj'
     inherit! :search_paths
     product_pods
 end
--- vl-iosTests/APIClientTests.swift
+++ vl-iosTests/APIClientTests.swift
@@ -7,27 +7,31 @@
 //
 
 import XCTest
+import OHHTTPStubs
+
+@testable import vl_ios;
 
 class APIClientTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSendUser() {
+        let user = User(country: "country",
+                        dateOfBirth: "10.04.1980",
+                        gender: "male",
+                        name: "name",
+                        placeOfBirth: "city",
+                        surname: "surname")
+        let apiClient = APIClient()
+        stub(condition: isMethodPOST()) { _ in
+            let stubPath = OHPathForFile("userResponse.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
         }
+        let expectation = self.expectation(description: "Send user API call")
+        apiClient.sendUser(user) { userResponse, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(userResponse)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 5.0, handler: nil)
     }
 
 }
